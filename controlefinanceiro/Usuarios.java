72d471f459b0ef0043cf27817eb1c273a46dcb42
S208 <S208@S0208>
2020-09-17 17:53:38 -0300

    1.3

--- controlefinanceiro/Usuarios.java
+++ controlefinanceiro/Usuarios.java
@@ -1,41 +1,5 @@
 package controlefinanceiro;
 
-public class Usuarios {
-	private int codigo;
-	private String nome;
-	private String senha;
-	
-	public boolean autenticaSenha(String senha) {
-		if(senha ==this.senha) {
-			return true;
-		}else
-			return false;
-	}
-	
-	// GETs e SETS
-	public int getCodigo() {
-		return codigo;		
-	}
-	public void setCodigo(int codigo) {
-		if (this.codigo<=0) {
-			System.out.println("Impossível cadastrar este código");
-			return;
-		
-	}}
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	public String getSenha() {
-		return senha;
-	}
-	public void setSenha(String senha) {
-		this.senha = senha;
-	}
-	
-// Fim classe Usuário
-	
+public class Usuarios extends Pessoas{
 
 }
--- controlefinanceiro/cadastroUsuario.java
+++ controlefinanceiro/cadastroUsuario.java
@@ -4,13 +4,6 @@ public class cadastroUsuario {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Usuarios teste = new Usuarios();
-		teste.setCodigo(-1);
-		teste.setNome("Pablo Vincenzi");
-		teste.setSenha("123456");
-		System.out.println(teste.getCodigo() + teste.getNome()+teste.getSenha());
-		boolean aut = teste.autenticaSenha("1234567");
-		System.out.println(aut);
 		
 
 	}
