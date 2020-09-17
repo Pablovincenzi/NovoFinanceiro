bf273fc3d6acf72622b02cfad4e67734ac34c424
S208 <S208@S0208>
2020-09-17 13:36:01 -0300

    1.1

---
+++ controlefinanceiro/teste.java
@@ -0,0 +1,24 @@
+package controlefinanceiro;
+
+import java.util.Date;
+import java.util.Scanner;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Scanner;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+class Teste {
+	public static void main(String[] args) {
+		try {
+			Scanner s = new Scanner(System.in);
+			System.out.println("Digite uma data: ");
+			String dataRecebida = s.nextLine();
+			DateFormat df = new SimpleDateFormat();
+			Date dt = df.parse(dataRecebida);
+			System.out.println(dt);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+}
